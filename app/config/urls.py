640431a6dac0eeaed62040ab09b85f10602923eb
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-18 07:44:49 +0900

    Category, Product 모델 추가

--- app/config/urls.py
+++ app/config/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from shop import views
 
 
@@ -22,3 +25,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        prefix=settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT,
+    )
+    urlpatterns += static(
+        prefix=settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )

df74ea343bfba5b5ad43213b4b8743730ee82baf
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-22 01:57:55 +0900

    장바구니 상품 개수 표시 기능 구현

--- app/config/settings.py
+++ app/config/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'shop.context_processors.menu_links',
+                'cart.context_processors.counter',
             ],
         },
     },

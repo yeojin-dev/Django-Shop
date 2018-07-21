6dd61a371217dcee95170138e73f5c44bf6825b9
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-21 22:59:59 +0900

    Paginator 적용

--- app/shop/views.py
+++ app/shop/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 from .models import Category, Product
 
@@ -11,16 +12,27 @@ def index(request):
 
 def all_products_by_category(request, category_slug=None):
     category_page = None
-    products = None
+    products_list = None
 
     if category_slug is not None:
         category_page = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(
+        products_list = Product.objects.filter(
             category=category_page,
             available=True,
         )
     else:
-        products = Product.objects.all().filter(available=True)
+        products_list = Product.objects.all().filter(available=True)
+
+    paginator = Paginator(products_list, 6)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+
+    try:
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
 
     context = {
         'category': category_page,
